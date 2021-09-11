7387abfed68aeb1212d163feede57a496b3c5faf
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-09-11 12:11:26 +0300

    Add jquery effects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,33 @@
 $(document).ready(function(){
+  $(".design").click(function(){
+    $(".design").slideUp("slow");
+    $("#hide1").slideToggle("slow");
+  });
+  $("#hide1").click(function(){
+    $("#hide1").fadeOut("slow");
+    $(".design").fadeIn("slow");
+    });
+  
+    $(".development").click(function(){
+      $(".development").slideUp("slow");
+      $("#hide2").slideToggle("slow");
+    });
+    $("#hide2").click(function(){
+      $("#hide2").fadeOut("slow");
+      $(".development").fadeIn("slow");
+      });
+
+      $(".management").click(function(){
+        $(".management").slideUp("slow");
+        $("#hide3").slideToggle("slow");
+      });
+      $("#hide3").click(function(){
+        $("#hide3").fadeOut("slow");
+        $(".management").fadeIn("slow");
+        });
+});
+
+$(document).ready(function(){
   $("form#user").submit(function(event) {
     event.preventDefault();
 
