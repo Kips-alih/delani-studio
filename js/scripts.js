f83358b34d9ac832d207afadb93637a22c32023c
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-09-12 19:12:16 +0300

    Add mouseleave events to the subtitles

--- js/scripts.js
+++ js/scripts.js
@@ -28,37 +28,62 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
+
   $(".work1").hover(function(){
   $(".subtitle1").show("slow");
   });
-
+  $(".work1").mouseleave(function(){
+    $(".subtitle1").hide("slow");
+  });
+  
   $(".work2").hover(function(){
     $(".subtitle2").show("slow");
+    });
+    $(".work2").mouseleave(function(){
+      $(".subtitle2").hide("slow");
     });
 
   $(".work3").hover(function(){
     $(".subtitle3").show("slow");
     });
+    $(".work3").mouseleave(function(){
+      $(".subtitle3").hide("slow");
+    });
 
   $(".work4").hover(function(){
     $(".subtitle4").show("slow");
+    });
+    $(".work4").mouseleave(function(){
+      $(".subtitle4").hide("slow");
     });
 
   $(".work5").hover(function(){
     $(".subtitle5").show("slow");
     });
+    $(".work5").mouseleave(function(){
+      $(".subtitle5").hide("slow");
+    });
 
   $(".work6").hover(function(){
     $(".subtitle6").show("slow");
+    });
+    $(".work6").mouseleave(function(){
+      $(".subtitle6").hide("slow");
     });
 
   $(".work7").hover(function(){
     $(".subtitle7").show("slow");
     });
+    $(".work7").mouseleave(function(){
+      $(".subtitle7").hide("slow");
+    });
 
   $(".work8").hover(function(){
     $(".subtitle8").show("slow");
-    });
+  });
+  $(".work8").mouseleave(function(){
+    $(".subtitle8").hide("slow");
+  });
 });
 
 $(document).ready(function(){
